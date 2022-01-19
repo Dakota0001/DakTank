ee33efe9b544883a78b6de6d521e96b71959288a
LiddulBOFH <13317534+LiddulBOFH@users.noreply.github.com>
2022-01-19 13:11:58 -0500

    Altered cl_distantsounds.lua
    Simplified how far sounds played, as well as made it collect fresh information when it comes time to play the sound in question
    Previously it collected the player position and calculated a distance from that to where the sound was coming from, and then played from that information after the delay; this led to incorrect sound positions if the player moved at any time between the sound event starting to the sound actually playing
    Also reverted to the older time delay

--- lua/dtte/cl_distantsounds.lua
+++ lua/dtte/cl_distantsounds.lua
@@ -18,19 +18,20 @@ net.Receive( "daktankexplosion", function()
 
 	local Dist = PlayerPos:Distance(ExplosionPos)
 	local Vol = math.Clamp(math.pow( 0.5,PlayerPos:Distance(ExplosionPos) / (ExplosionPower * 50) ),0,1)
-	local SoundPos = LerpVector(0.4,PlayerPos,ExplosionPos)
 	if Vol > 0.01 then
-		timer.Simple(Dist / 18005, function()
-			if Dist < 8192 then
-				local LerpScale = math.min((Dist / 8192) * 1.5,1)
-				sound.Play( ExplosionSound, LerpVector(LerpScale,ExplosionPos,SoundPos), math.min(110 + (ExplosionPower / 7),165), 100, Vol)
-			else
-				local LerpScale = math.min(Dist / 32768,0.5)
-				sound.Play( ExplosionSound, LerpVector(LerpScale,SoundPos,PlayerPos), math.min(110 + (ExplosionPower / 7),165), 100, Vol)
+		timer.Simple(Dist / 13503.9, function()
+
+			local DistFresh = PlayerPos:Distance(ExplosionPos)
+			local PlayerPosFresh = LocalPlayer():EyePos()
+			local SoundDir = (ExplosionPos - PlayerPosFresh):GetNormalized()
+			local SoundPos = PlayerPosFresh + (SoundDir * math.Clamp(DistFresh,0,4096))
+
+			for i = 1,math.ceil(math.max(ExplosionPower / 80,1)) do
+				sound.Play( ExplosionSound, SoundPos, math.min(110 + (ExplosionPower / 7),165), 100, Vol)
 			end
 
 			if LocalPlayer():GetMoveType() ~= MOVETYPE_NOCLIP then
-				local Force = (ExplosionPower^1.2) / (Dist * 0.1)
+				local Force = (ExplosionPower^1.2) / (DistFresh * 0.1)
 				local Amp = (ExplosionPower / 10)
 				if (Force * 10) > 5 then util.ScreenShake( ExplosionPos, Amp * 1.2, Amp * 0.74, (ExplosionPower * 0.6) * 0.015, ExplosionPower^1.35 ) end
 			end
@@ -39,23 +40,26 @@ net.Receive( "daktankexplosion", function()
 end )
 
 net.Receive( "daktankshotfired", function()
-	local PlayerPos = LocalPlayer():EyePos()
 	local GunPos = net.ReadVector()
 	local Caliber = net.ReadFloat()
 	local GunSound = net.ReadString()
+	local PlayerPos = LocalPlayer():EyePos()
 	local Dist = PlayerPos:Distance(GunPos)
 	local Vol = math.Clamp(math.pow( 0.5,PlayerPos:Distance(GunPos) / (Caliber * 250) ),0,1)
-	local SoundPos = LerpVector(0.4,PlayerPos,GunPos)
+
 	if Vol > 0.01 then
-		timer.Simple(Dist / 18005, function()
-			if Dist < 8192 then
-				local LerpScale = math.min((Dist / 8192) * 1.5,1)
-				sound.Play( GunSound, LerpVector(LerpScale,GunPos,SoundPos), math.min(90 + (Caliber / 3),165), 100 * math.Rand(0.95,1.05), Vol)
-			else
-				local LerpScale = math.min(Dist / 32768,0.5)
-				sound.Play( GunSound, LerpVector(LerpScale,SoundPos,PlayerPos), math.min(90 + (Caliber / 3.5),165), 100 * math.Rand(0.95,1.05), Vol)
+		timer.Simple(Dist / 13503.9, function()
+
+			local PlayerPosFresh = LocalPlayer():EyePos()
+			local DistFresh = PlayerPosFresh:Distance(GunPos)
+			local SoundDir = (GunPos - PlayerPosFresh):GetNormalized()
+			local SoundPos = PlayerPosFresh + (SoundDir * math.Clamp(DistFresh,0,4096))
+
+			for i = 1,math.ceil(math.max(Caliber / 75,1)) do
+				sound.Play( GunSound, SoundPos, math.min(110 + (Caliber / 3),165), 100, Vol)
 			end
-			local Force = (35 * Caliber) / (Dist / 1.5)
+
+			local Force = (35 * Caliber) / (DistFresh / 2)
 			if LocalPlayer():GetMoveType() ~= MOVETYPE_NOCLIP and Force > 10 then
 				util.ScreenShake( GunPos, Force / 10, Force / 2, math.min(Force / 80,0.6), Force * 2.5 )
 			end
